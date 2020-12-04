478fbbd2ecabf60ba9c386269cd45d383ce9b35a
Grigore Stefan <g_stefan@yahoo.com>
2020-12-04 13:57:33 +0200

    update

--- port/build.msvc.clean-release.cmd
+++ port/build.msvc.clean-release.cmd
@@ -3,6 +3,6 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo -^> clean-local-release time-cmd
+echo -^> clean-release time-cmd
 
 if exist release\ rmdir /Q /S release
--- port/build.platform.github-release-check.cmd
+++ port/build.platform.github-release-check.cmd
@@ -5,7 +5,7 @@ rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-echo -^> git-release-check time-cmd
+echo -^> github-release-check time-cmd
 
 set PROJECT=time-cmd
 if not exist source\%PROJECT%.version.ini echo Error - no version & exit 1
