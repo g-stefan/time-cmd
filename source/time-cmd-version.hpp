368d960f507ca90f106b23cb1d1014e102dabb9f
Grigore Stefan <g_stefan@yahoo.com>
2021-07-06 20:22:16 +0300

    update

--- source/time-cmd-version.hpp
+++ source/time-cmd-version.hpp
@@ -10,10 +10,10 @@
 #ifndef TIME_CMD_VERSION_HPP
 #define TIME_CMD_VERSION_HPP
 
-#define TIME_CMD_VERSION_ABCD                1,5,0,6
-#define TIME_CMD_VERSION_STR                 "1.5.0"
-#define TIME_CMD_VERSION_STR_BUILD           "6"
-#define TIME_CMD_VERSION_STR_DATETIME        "2021-03-15 12:30:20"
+#define TIME_CMD_VERSION_ABCD                1,6,0,7
+#define TIME_CMD_VERSION_STR                 "1.6.0"
+#define TIME_CMD_VERSION_STR_BUILD           "7"
+#define TIME_CMD_VERSION_STR_DATETIME        "2021-07-06 15:11:44"
 
 #ifndef XYO_RC
 
